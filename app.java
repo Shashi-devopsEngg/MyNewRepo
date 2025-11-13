5a13d91c974d65c65ad8103c5e8baaaa4539c6be
Shashi-devopsEngg <shashikanthreddy045@gmail.com>
2025-11-13 10:16:32 +0530

    Update app.java

--- app.java
+++ app.java
@@ -20,3 +20,18 @@ public class VariablesExample {
         System.out.println("Active: " + isActive);
     }
 }
+
+#Conditional Statements (if-else):
+public class ConditionalExample {
+    public static void main(String[] args) {
+        int score = 85;
+
+        if (score >= 90) {
+            System.out.println("Grade: A");
+        } else if (score >= 80) {
+            System.out.println("Grade: B");
+        } else {
+            System.out.println("Grade: C or lower");
+        }
+    }
+}
